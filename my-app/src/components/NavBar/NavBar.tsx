b5c55566f21be6267e7b111b3d9f8714aa30ea14
Rohan Almeida <rohanalmeida@live.com>
2023-01-24 22:10:22 +0530

    Added Nav Bar

---
+++ my-app/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,143 @@
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Button from "@mui/material/Button";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  color: "black",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.05),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.black, 0.15),
+  },
+  marginRight: theme.spacing(1),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+const test = [
+  {
+    label: "test1",
+    onclick: () => {
+      console.log("hi");
+    },
+  },
+  {
+    label: "test2",
+    onclick: () => {
+      console.log("hi");
+    },
+  },
+  {
+    label: "test3",
+    onclick: () => {
+      console.log("hi");
+    },
+  },
+];
+
+function NavBar() {
+  return (
+    <Box sx={{ width: "100vw" }}>
+      <AppBar position="absolute">
+        <Toolbar sx={{ justifyContent: "space-between", bgcolor: "white" }}>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <IconButton
+              size="large"
+              edge="start"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              color="black"
+              component="div"
+              mr="10px"
+              sx={{ display: { sm: "block" } }}
+            >
+              Jira Clone
+            </Typography>
+            {test.map((link) => (
+              <Button
+                variant="text"
+                onClick={link.onclick}
+                sx={{ color: "black" }}
+              >
+                {link.label}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+            <IconButton size="medium">
+              <Badge badgeContent={0} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton onClick={() => {}}>
+              <AccountCircle />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export { NavBar };
