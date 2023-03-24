5c4d28f068fb7f5cfdfbcbabfd610ea6d48f3364
rajeev <rajeev.dessai11@gmail.com>
2023-03-24 15:31:42 +0530

    docker file for backend,frontend and db

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ from webapp import app
 import datetime
 app.config['SECRET_KEY'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'  # secret key
 app.config['MONGO_URI'] = 'mongodb+srv://fury:RP8qV1c6d6LZ7xjM@portfolio.qmqe2.mongodb.net/test'  # uri
-app.config['MONGO_URI'] = 'mongodb+srv://fury:RP8qV1c6d6LZ7xjM@portfolio.qmqe2.mongodb.net/test'  # uri
+# app.config['MONGO_URI'] = 'mongodb+srv://fury:RP8qV1c6d6LZ7xjM@portfolio.qmqe2.mongodb.net/test'  # uri
 app.config['JWT_ACCESSS_TOKEN_EXPIRES'] = datetime.timedelta(days=300)
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
---
+++ scripts/database/init.js
@@ -0,0 +1,18 @@
+db.createUser({
+    user: 'appUser',
+    pwd: 'test',
+    roles: [
+        {
+            role: 'readWrite',
+            db: 'testDB',
+        },
+    ],
+});
+
+db = new Mongo().getDB("testDB");
+
+db.user_details.insert({
+    username:"test",
+    password:'$2b$12$Z0h/vt43WrmIlYuFX/VzieWTAagc/UqHvcUDZMDlOhm7qrVr3Uk5O',
+})
+
