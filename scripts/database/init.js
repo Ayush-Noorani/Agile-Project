0a264dc4450cb4ad70b53c2ea9c4dde1d46032ab
fury-r <rajeev.dessai11@gmail.com>
2023-03-24 22:01:24 +0530

    change in config,init.js,axios.ts

--- scripts/database/init.js
+++ scripts/database/init.js
@@ -13,6 +13,6 @@ db = new Mongo().getDB("testDB");
 
 db.user_details.insert({
     username:"test",
-    password:'$2b$12$Z0h/vt43WrmIlYuFX/VzieWTAagc/UqHvcUDZMDlOhm7qrVr3Uk5O',
+    password: Buffer.from('$2b$12$Z0h/vt43WrmIlYuFX/VzieWTAagc/UqHvcUDZMDlOhm7qrVr3Uk5O', 'utf-8'),
 })
 
