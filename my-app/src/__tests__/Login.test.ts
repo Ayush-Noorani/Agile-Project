ca668b9afab71863dbaff112a6ae076565770d9e
Rajeev Dessai <79844581+fury-r@users.noreply.github.com>
2023-03-27 07:15:37 +0530

    Feature/jest test (#33)
    * Configured Jest and Wrote test Cases for login
    
    * Configured Jest and Wrote test Cases for login
    
    ---------
    
    Co-authored-by: Rohan Almeida <rohanalmeida@live.com>

---
+++ my-app/src/__tests__/Login.test.ts
@@ -0,0 +1,60 @@
+// import { axiosInstance } from "../helper/axios";
+import axios from "axios";
+
+axios
+
+const axiosInstance = axios.create({
+  baseURL: "http://0.0.0.0:8000"
+})
+
+test("Backend Up and Running", async () => {
+  const result = await axiosInstance.get("/")
+  expect(result.data).toStrictEqual({'message':'Hello, World!'})
+  expect(result.status).toBe(200);
+});
+
+
+test("Correct Email and Correct Password",async()=>{
+  const value = {
+    name: "rajeev.dessai11@gmail.com",
+    password: "test"
+  }
+  const result = await axiosInstance.post("/user/login",value);
+  expect(result.data.data).toBe("User found - Login successful");
+  expect(result.status).toBe(200)
+})
+
+
+
+test("Wrong Email and Correct Password",async()=>{
+ const value = {
+    name: "aBrokenEmail@gmail.com",
+    password: "test"
+  };
+
+  await axiosInstance.post("/user/login",value).catch(err=>{
+    expect(err.message).toBe("Request failed with status code 400")
+  })
+})
+
+test("Correct Email and Wrong Password",async()=>{
+  const value = {
+     name: "rajeev.dessai11@gmail.com",
+     password: "test123"
+   };
+ 
+   await axiosInstance.post("/user/login",value).catch(err=>{
+     expect(err.message).toBe("Request failed with status code 400")
+   })
+ })
+
+ test("Wrong Email and Wrong Password",async()=>{
+  const value = {
+     name: "yeeeBoi@",
+     password: "test123"
+   };
+ 
+   await axiosInstance.post("/user/login",value).catch(err=>{
+     expect(err.message).toBe("Request failed with status code 400")
+   })
+ })
