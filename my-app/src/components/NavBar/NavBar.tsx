5762e90939ae9588df15a31df4fc2fd483c2037a
Ayush <ayushnoorani6@gmail.com>
2023-04-02 11:04:44 +0530

    Fixes

--- my-app/src/components/NavBar/NavBar.tsx
+++ my-app/src/components/NavBar/NavBar.tsx
@@ -234,7 +234,7 @@ const NavBar = ({ show, setShow }: NavBarProps) => {
               mr="10px"
               sx={{ display: { sm: "block" } }}
             >
-              ProjectCentral
+              SprintBoard
             </Typography>
             {navBarItems.map((link, index) =>
               user.roles.includes("admin") ? (
