3cd9941a92a9e43689d235e108d0bf5de45aa41d
fury-r <rajeev.dessai11@gmail.com>
2023-04-23 20:21:49 +0530

    add more sections in report

--- my-app/src/pages/PieChart/PieChart.tsx
+++ my-app/src/pages/PieChart/PieChart.tsx
@@ -1,12 +1,30 @@
-import { Box } from "@mui/material";
+import { Accordion, AccordionSummary, Box, InputLabel } from "@mui/material";
 import "chart.js/auto";
-import { Pie } from "react-chartjs-2";
+import { Line, Pie } from "react-chartjs-2";
 import React, { useEffect, useState } from "react";
 import { useTask } from "../Task/hooks/useTask";
 import "./pieChart.css";
 import Colors from "../../helper/colors";
 import { useParams } from "react-router-dom";
-
+import "../../css/common.css";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+const chartData = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [12, 19, 3, 5, 2, 3, 7],
+      borderColor: "red",
+      fill: false,
+    },
+    {
+      label: "Dataset 2",
+      data: [2, 3, 20, 5, 1, 4, 12],
+      borderColor: "blue",
+      fill: false,
+    },
+  ],
+};
 const PieChart = ({ id, projectName, closeHandler }: any) => {
   const [pieData, setPieData] = useState<number[]>([]);
   const [pieLabels, setPieLabels] = useState<string[]>([]);
@@ -60,14 +78,35 @@ const PieChart = ({ id, projectName, closeHandler }: any) => {
   };
 
   return (
-    <Box className={"parentWrapper"}>
+    <Box className={"parentWrapper tertiary"}>
       <Box className={"contentWrapper"}>
         <Box className={"headerWrapper"}>
           <h3>Project status for {currentProject?.name}</h3>
         </Box>
-        <Box className={"chartWrapper"}>
-          <Pie data={data} options={options} />
-        </Box>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <h4 className="font-bold">Tasks Overview</h4>
+          </AccordionSummary>
+          <Box className={"chartWrapper "}>
+            <Pie data={data} options={options} />
+          </Box>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <h4 className="font-bold">Plans Overview</h4>
+          </AccordionSummary>
+          <Box className={"chartWrapper "}>
+            <Line data={chartData} options={options} />
+          </Box>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <h4 className="font-bold">User Overview</h4>
+          </AccordionSummary>
+          <Box className={"chartWrapper "}>
+            <Line data={chartData} options={options} />
+          </Box>
+        </Accordion>
       </Box>
     </Box>
   );
