6dd93626af4782535e10b8fc071b9c262ecace31
fury-r <rajeev.dessai11@gmail.com>
2023-04-30 16:56:21 +0530

    move functions in utils

--- my-app/src/utils/Common.ts
+++ my-app/src/utils/Common.ts
@@ -73,3 +73,16 @@ export const colors = {
   tertiary: "#DDDDDD",
   dark: "#222831",
 };
+
+export const getNameAcronym = (name: string) => {
+  const words = name.split(" ");
+  const acronym = words.reduce((result, word) => result + word[0], "");
+  return acronym.toUpperCase();
+};
+
+export const generateRandomColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+};
